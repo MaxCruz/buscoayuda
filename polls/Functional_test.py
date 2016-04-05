cd13065a1949c33e92372e70c3b09e3f7fa83694
Max Cruz <max.raul@gmail.com>
2016-04-04 22:07:13 -0500

    Paso 2 - Prueba del registro de un independiente (Rojo)

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -1,22 +1,57 @@
+from selenium.webdriver.common.by import By
+
 __author__ = 'Max'
 
 from unittest import TestCase
 from selenium import webdriver
 
+
 class FunctionalTest(TestCase):
-
-
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-
     def tearDown(self):
         self.browser.quit()
-
 
     def test_title(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda', self.browser.title)
 
+    def test_registro(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_register')
+        link.click()
 
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Max')
 
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Cruz')
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('5')
+
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrolador Web']").click()
+
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('3192829171')
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('mr.cruz@uniandes.edu.co')
+
+        imagen = self.browser.find_element_by_id('id_imagen')
+        imagen.send_keys('/Users/max/Pictures/Wallpapers/pi_2-1440x900.jpg')
+
+        nombreUsuario = self.browser.find_element_by_id('id_username')
+        nombreUsuario.send_keys('max')
+
+        clave = self.browser.find_element_by_id('id_password')
+        clave.send_keys('clave123')
+
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+
+        self.browser.implicitly_wait(3)
+        span=self.browser.find_element(By.XPATH, '//span[text()="Max Cruz"]')
+
+        self.assertIn('Max Cruz', span.text)
